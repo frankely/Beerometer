15721572fe481a91cbd9e69c884363b45b59e59a
Frankely Diaz <diazfrankely@gmail.com>
2015-03-16 16:06:25 -0400

    Styles

--- app/src/main/java/frankelydiaz/me/beerometer/HomeActivity.java
+++ app/src/main/java/frankelydiaz/me/beerometer/HomeActivity.java
@@ -1,15 +1,13 @@
 package frankelydiaz.me.beerometer;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class HomeActivity extends ActionBarActivity {
@@ -18,11 +16,14 @@ public class HomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+
     }
 
 
