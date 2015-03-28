897c85ab9c281e647a0c6966ceaff7008f2d250a
Frankely Diaz <diazfrankely@gmail.com>
2015-03-28 10:54:58 -0400

    Cambios del Feature 1

---
+++ app/src/androidTest/java/me/frankelydiaz/beerometer/test/TestUtilities.java
@@ -0,0 +1,72 @@
+package me.frankelydiaz.beerometer.test;
+
+/**
+ * Created by frankelydiaz on 3/28/15.
+ */
+
+import android.content.ContentValues;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.test.AndroidTestCase;
+
+import java.util.Map;
+import java.util.Set;
+
+/*
+    Students: These are functions and some test data to make it easier to test your database and
+    Content Provider.  Note that you'll want your WeatherContract class to exactly match the one
+    in our solution to use these as-given.
+ */
+public class TestUtilities {
+
+    public static void validateCursor(String error, Cursor valueCursor, ContentValues expectedValues) {
+        AndroidTestCase.assertTrue("Empty cursor returned. " + error, valueCursor.moveToFirst());
+        validateCurrentRecord(error, valueCursor, expectedValues);
+        valueCursor.close();
+    }
+
+    public static void validateCurrentRecord(String error, Cursor valueCursor, ContentValues expectedValues) {
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet) {
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            AndroidTestCase.assertFalse("Column '" + columnName + "' not found. " + error, idx == -1);
+            String expectedValue = entry.getValue().toString();
+            AndroidTestCase.assertEquals("Value '" + entry.getValue().toString() +
+                    "' did not match the expected value '" +
+                    expectedValue + "'. " + error, expectedValue, valueCursor.getString(idx));
+        }
+    }
+
+    static class TestContentObserver extends ContentObserver {
+        final HandlerThread mHT;
+        boolean mContentChanged;
+
+        static TestContentObserver getTestContentObserver() {
+            HandlerThread ht = new HandlerThread("ContentObserverThread");
+            ht.start();
+            return new TestContentObserver(ht);
+        }
+
+        private TestContentObserver(HandlerThread ht) {
+            super(new Handler(ht.getLooper()));
+            mHT = ht;
+        }
+
+        // On earlier versions of Android, this onChange method is called
+        @Override
+        public void onChange(boolean selfChange) {
+            onChange(selfChange, null);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            mContentChanged = true;
+        }
+
+    }
+
+}
